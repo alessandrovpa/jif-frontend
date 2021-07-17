592f42a21f76cf839474f175706f11e0a867d99b
Alessandro <alessandrovictor157@gmail.com>
2021-07-17 02:49:20 -0300

    testind router basename

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import AppProvider from './context';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter basename="https://jifnacional.ifgoiano.edu.br">
+    <BrowserRouter>
       <AppProvider>
         <Routes />
 
